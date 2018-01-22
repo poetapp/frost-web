ae43a60e462353d9331ce40dad391b1c8bd37710
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2018-01-22 13:19:06 -0300

    add link header

--- src/components/atoms/LinksHeader/LinksHeader.tsx
+++ src/components/atoms/LinksHeader/LinksHeader.tsx
@@ -7,7 +7,7 @@ interface LinksHeaderProps {
 
 export const LinksHeader = (props: LinksHeaderProps) =>
     <ul className={'LinksHeader'}>
-        <li className={'LinksHeader__item'}><a href={''}>Documentation</a></li>
-        <li className={'LinksHeader__item'}><a href={''}>API</a></li>
-        <li className={'LinksHeader__item'}><a href={''}>Forum</a></li>
+        <li className={'LinksHeader__item'}><a href={'https://frost.readme.io/docs'}>Documentation</a></li>
+        <li className={'LinksHeader__item'}><a href={'https://frost.readme.io/v0.1/reference'}>API</a></li>
+        <li className={'LinksHeader__item'}><a href={'https://frost.readme.io/discuss'}>Forum</a></li>
     </ul>
