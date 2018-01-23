fa23329b7ca98461f72feeb5ea2e84b016fdf81d
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2018-01-23 17:28:17 -0300

    add reducer for change password

---
+++ src/reducers/ChangePassword.reducer.ts
@@ -0,0 +1,42 @@
+import { Actions } from '../actions/index'
+
+const defaultState = {
+  error: {
+    status: false,
+    message: ''
+  },
+  loading: false
+}
+
+export const changePassword = (state: any, action: any) => {
+  switch (action.type) {
+    case Actions.ChangePassword.CHANGE_PASSWORD:
+      return {
+        ...state,
+        error: {
+          status: false,
+          message: ''
+        },
+        loading: true
+      }
+    case Actions.ChangePassword.CHANGE_PASSWORD_SUCCESS:
+      return {
+        ...state,
+        error: {
+          status: false,
+          message: ''
+        },
+        loading: false
+      }
+    case Actions.ChangePassword.CHANGE_PASSWORD_ERROR:
+      return {
+        ...state,
+        error: {
+          status: true,
+          message: action.payload
+        },
+        loading: false
+      }
+  }
+  return state || defaultState
+}
