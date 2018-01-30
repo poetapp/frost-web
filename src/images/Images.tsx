f6bed5335763524a9fa9df6471db2175c4f1e348
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2018-01-30 12:08:06 -0300

    favicon

--- src/images/Images.tsx
+++ src/images/Images.tsx
@@ -3,4 +3,5 @@ export class Images {
   static readonly Toogle = require('./toggle.svg')
   static readonly PoetWhite = require('./poet-white.svg')
   static readonly FrostLogoOnly = require('./frost-logo-only.svg')
+  static readonly FavIcon = require('./fav.ico')
 }
