d33de36245b7efe547a215ddef41839c511d1a8a
Lautaro Dragan <lautaro.dragan@gmail.com>
2018-02-12 01:03:08 -0300

    Mini refactor

--- src/components/molecules/PanelOptions/PanelOptions.tsx
+++ src/components/molecules/PanelOptions/PanelOptions.tsx
@@ -7,34 +7,32 @@ import './PanelOptions.scss'
 interface PanelOptionsProps {}
 
 export const PanelOptions = (props: PanelOptionsProps) => (
-  <div className={'PanelOptions'}>
-    <section className={'PanelOptions__actions'}>
-      <div className={'PanelOptions__actions__welcome'}>
-        <h2>Welcome to Frost</h2>
-      </div>
-      <div className={'PanelOptions__actions__box-buttons'}>
-        <Link to={'/token'}>
-          <BoxButton
-            image={Images.KeySmall}
-            title={'API Keys'}
-            description={
-              'Get started building out an integration on top of po.et using the frost API.'
-            }
-            buttonText={'Get API Key'}
-          />
-        </Link>
+  <main className={'PanelOptions'}>
+    <div className={'PanelOptions__welcome'}>
+      <h2>Welcome to Frost</h2>
+    </div>
+    <div className={'PanelOptions__box-buttons'}>
+      <Link to={'/token'}>
+        <BoxButton
+          image={Images.KeySmall}
+          title={'API Keys'}
+          description={
+            'Get started building out an integration on top of po.et using the frost API.'
+          }
+          buttonText={'Get API Key'}
+        />
+      </Link>
 
-        <a href="https://docs.frost.po.et/docs" target="_blank">
-          <BoxButton
-            image={Images.Book}
-            title={'Documentation & Guides'}
-            description={
-              'View a list of getting started guides and full API documentation for interacting with the Po.et Network. '
-            }
-            buttonText={'View Docs'}
-          />
-        </a>
-      </div>
-    </section>
-  </div>
+      <a href="https://docs.frost.po.et/docs" target="_blank">
+        <BoxButton
+          image={Images.Book}
+          title={'Documentation & Guides'}
+          description={
+            'View a list of getting started guides and full API documentation for interacting with the Po.et Network. '
+          }
+          buttonText={'View Docs'}
+        />
+      </a>
+    </div>
+  </main>
 )
