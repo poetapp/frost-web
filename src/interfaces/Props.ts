fc5af1b3b5f1e022e3cd4259d6ab0e3bd789ebbb
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2018-02-27 12:22:03 -0300

    add interface FrostState, User, Profile, ApiToken

--- src/interfaces/Props.ts
+++ src/interfaces/Props.ts
@@ -1,3 +1,21 @@
 export interface ClassNameProps {
   readonly className?: string
 }
+export interface ApiToken {
+  apiToken: string
+  dateCreated: string
+}
+export interface Profile {
+  email: string
+  apiToken?: ApiToken
+  verified: boolean
+  createdAt: string
+}
+export interface User {
+  token: string | undefined
+  profile: Profile
+}
+
+export interface FrostState {
+  user: User
+}
