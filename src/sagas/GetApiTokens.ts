fd2b62d2a59dc9bc8d41da1dbdd2534b82520eae
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2018-03-13 22:30:35 -0300

    Support for Multiple API Tokens (#24)

--- src/sagas/GetApiTokens.ts
+++ src/sagas/GetApiTokens.ts
@@ -1,6 +1,13 @@
 import { Frost } from '@poetapp/frost-client'
+import * as moment from 'moment'
 import { call, takeLatest, put } from 'redux-saga/effects'
 import { Actions } from '../actions/index'
+
+const parseJwt = (token: string) => {
+  const base64Url = token.split('.')[1]
+  const base64 = base64Url.replace('-', '+').replace('_', '/')
+  return JSON.parse(window.atob(base64))
+}
 
 async function GetApiTokensFrost(apiToken: string) {
   const frost = new Frost({ host: '/api' })
@@ -21,7 +28,18 @@ function* GetApiTokens(action: any) {
   try {
     const { token } = action.payload
     const tokens = yield call(GetApiTokensFrost, token)
-    yield put(Actions.ApiTokens.onGetApiTokensSuccess(tokens))
+    const apiTokens = tokens.apiTokens.map((token: string) => {
+      const jwtDecoded = parseJwt(token)
+
+      return {
+        token,
+        dateCreated: moment.unix(jwtDecoded.iat).format('MM/DD/YYYY hh:mm a'),
+        expiration: moment.unix(jwtDecoded.exp).format('MM/DD/YYYY hh:mm a'),
+        isExpired: !moment(moment.now()).isAfter(jwtDecoded.exp)
+      }
+    })
+
+    yield put(Actions.ApiTokens.onGetApiTokensSuccess(apiTokens))
   } catch (e) {
     yield put(Actions.ApiTokens.onGetApiTokensError(e))
     // Todo: Error message in UI
