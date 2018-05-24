7d4f77e936c0d363199c2b985e9d1c8baaf9bce0
jwicks31 <jessewicks@gmail.com>
2018-05-24 12:39:24 -0500

    added storybook to frost-web

---
+++ src/components/atoms/Button/Button.stories.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+import { Button } from './Button'
+import { wInfo } from '../../../stories/index.stories'
+import { text, boolean } from '@storybook/addon-knobs/react'
+
+
+
+storiesOf('Components/Button', module).addWithJSX(
+  'Basic Button',
+  wInfo(`
+
+  ### Notes
+
+  This is a button
+
+  ### Usage
+  ~~~js
+  <Button
+    readonly className?: string
+    readonly text?: string
+    readonly disabled?: boolean
+    readonly onClick?: (event: Event) => void
+    readonly type?: 'primary' | 'danger'
+    />
+  ~~~`
+)(() => (
+    <Button
+      text={text('text', 'Test Button')}
+      disabled={boolean('disabled', false)}
+      type={text('type','primary')}
+      onClick={() => alert('clicked')}
+      className={text('className')}
+     />
+  ))
+)
