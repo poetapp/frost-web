5a699dfb8a51bf9852a3605c58644127aa329408
jwicks31 <jessewicks@gmail.com>
2018-05-24 17:45:12 -0500

    trailing commas

--- src/stories/index.stories.js
+++ src/stories/index.stories.js
@@ -11,21 +11,21 @@ import { text, boolean } from '@storybook/addon-knobs/react'
       h1: {
         marginRight: '20px',
         fontSize: '25px',
-        display: 'inline'
+        display: 'inline',
       },
       body: {
         paddingTop: 0,
-        paddingBottom: 0
+        paddingBottom: 0,
       },
       h2: {
         display: 'inline',
-        color: '#999'
+        color: '#999',
       }
     },
     infoBody: {
       backgroundColor: '#eee',
       padding: '0px 5px',
-      lineHeight: '2'
+      lineHeight: '2',
     }
   }
   export const wInfo = text => withInfo({ inline: true, source: false, styles: wInfoStyle, text: text })
