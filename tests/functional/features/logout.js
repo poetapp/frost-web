47a39461519d41d379e4f1274b8192ddf5684ca7
warren vosper <warren.vosper@po.et>
2018-08-13 14:11:44 -0600

    updates test description

--- tests/functional/features/logout.js
+++ tests/functional/features/logout.js
@@ -5,7 +5,7 @@ import { DashboardPage } from '../page-objects/dashboard-page'
 fixture `User logout`
   .page `${SITE}${pages.DASHBOARD}`
 
-test.only(`The logout button on the ${pages.DASHBOARD} page signs a user out`, async t => {
+test(`The logout button on the ${pages.DASHBOARD} page signs a user out`, async t => {
   const should = `Should load the dashboard page at ${pages.DASHBOARD}`
   const expected = pages.LOGIN
   const { email, password } = await LoginRegisterPage.createUser(t)
