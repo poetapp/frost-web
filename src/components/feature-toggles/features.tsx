386804306f3e65a736af28ce879e01ff28fbcb4d
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2018-09-06 17:09:32 -0300

    fix lint

--- src/components/feature-toggles/features.tsx
+++ src/components/feature-toggles/features.tsx
@@ -1,8 +1,8 @@
 import { Feature } from '@paralleldrive/react-feature-toggles'
 import { Toggle } from 'components/atoms/Toggle/Toggle'
 import { ToggleNetworkContainer } from 'components/containers/ToggleNetwork.container'
+import { FeatureName } from 'config/features'
 import * as React from 'react'
-import { FeatureName } from './initialFeatures'
 
 export const toggleNetwork = () => (
   <Feature name={FeatureName.ToggleNetwork} inactiveComponent={Toggle} activeComponent={ToggleNetworkContainer} />
