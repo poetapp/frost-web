6e860f9229eccae394deb0eca95176c92cd5083e
Geoff Turk <geoff.turk@po.et>
2018-09-27 22:02:07 +0200

    Fix wording on button

--- src/components/molecules/PanelOptions/PanelOptions.tsx
+++ src/components/molecules/PanelOptions/PanelOptions.tsx
@@ -18,7 +18,7 @@ export const PanelOptions = (props: PanelOptionsProps) => (
           image={Images.KeySmall}
           title={'API Tokens'}
           description={'Get started building out an integration on top of po.et using the frost API.'}
-          buttonText={'Create API Token'}
+          buttonText={'Get API Token'}
         />
       </Link>
 
