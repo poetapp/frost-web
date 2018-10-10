4c99a690e4fb6408524444bc4adf801f77494cde
Jesse Wicks <jessewicks@gmail.com>
2018-10-09 19:24:44 -0500

    rename Network Interface

--- src/components/atoms/ToggleMainnet/ToggleMainnet.tsx
+++ src/components/atoms/ToggleMainnet/ToggleMainnet.tsx
@@ -11,7 +11,7 @@ interface ToggleMainnetProps extends ClassNameProps {
   readonly onToogle?: (network: Network) => void
 }
 
-export const isNetworkMainnet = (enabledMainnet: boolean) => (enabledMainnet ? Network.MAINNET : Network.TESTNET)
+export const isNetworkMainnet = (enabledMainnet: boolean) => (enabledMainnet ? Network.LIVE : Network.TEST)
 
 const onToogle = (event: Event, callback: (network: Network) => void) => {
   const checkbox = event.target as HTMLInputElement
