3328f68db394d23a0dd91074dc6aebd2b8e4fa29
Wesley Charles Blake <wesley@po.et>
2018-10-17 15:58:18 +0200

    frost-web for QA

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const CircularDependencyPlugin = require('circular-dependency-plugin')
 const validEnvironments = [
   'development',
   'testing',
-  'staging',
+  'qa',
   'production',
 ]
 const environment = process.env.POET_ENV || 'development'
@@ -17,7 +17,7 @@ const environment = process.env.POET_ENV || 'development'
 assert(validEnvironments.includes(environment), `Invalid value for POET_ENV: ${environment}. Valid values are: ${validEnvironments}`)
 
 const production = environment === 'production'
-const staging = environment === 'staging'
+const qa = environment === 'qa'
 const development = environment === 'development'
 const testing = environment === 'testing'
 const configurationPath = `./env/${environment}.json`
@@ -115,7 +115,7 @@ module.exports = {
     publicPath: "/"
   },
 
-  devtool: production || testing || staging ? '' : 'eval',
+  devtool: production || testing || qa ? '' : 'eval',
 
   resolve: {
     extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js', '.json', '.css', '.scss'],
