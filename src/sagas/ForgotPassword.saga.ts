82704e75e70c9c61cef06c65a2e74995103950ca
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2018-10-17 17:12:46 -0300

    feat: Show Toast message for Forgot Password actions (#368)

--- src/sagas/ForgotPassword.saga.ts
+++ src/sagas/ForgotPassword.saga.ts
@@ -2,6 +2,7 @@ import { Frost } from '@po.et/frost-client'
 import { Actions } from 'actions/index'
 import { delay, SagaIterator } from 'redux-saga'
 import { call, takeLatest, put, ForkEffect } from 'redux-saga/effects'
+const { toast } = require('react-toastify')
 
 async function GetApiTokensFrost(email: string): Promise<string> {
   const frost = new Frost({ host: '/api' })
@@ -22,8 +23,18 @@ function* ForgotPassword(action: any): SagaIterator {
     yield put(Actions.ForgotPassword.onForgotPasswordSuccess())
     yield put(Actions.LoadingPage.onLoadingFull())
     yield call(delay, 300)
+
+    toast.success('Please verify your email account', {
+      className: 'toast',
+      autoClose: 2500,
+    })
   } catch (e) {
     yield put(Actions.LoadingPage.onLoadingFull())
     yield put(Actions.ForgotPassword.onForgotPasswordError(e))
+
+    toast.error(e, {
+      className: 'toast',
+      autoClose: 2500,
+    })
   }
 }
