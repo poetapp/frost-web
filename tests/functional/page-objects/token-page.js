60a3637c47a3586d1e586781364a72fc38705119
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2018-10-18 14:00:41 -0300

    fix

--- tests/functional/page-objects/token-page.js
+++ tests/functional/page-objects/token-page.js
@@ -3,7 +3,7 @@ import { Selector } from 'testcafe'
 const PAGE_CLASS = '.CreateToken'
 const pageClass = Selector(PAGE_CLASS)
 const apiTokens = Selector(`${PAGE_CLASS} .BoxToken__item`)
-const addTokenAction = Selector(`${PAGE_CLASS} .CreateToken__button`)
+const addTokenAction = Selector(`${PAGE_CLASS} .CreateTokenContainer__CreateToken__button`)
 const deleteTokenAction = Selector(`${PAGE_CLASS} .BoxToken__item__actions`)
 const deleteTokenConfirm = Selector(`${PAGE_CLASS} .DeleteToken .Button__danger`)
 
