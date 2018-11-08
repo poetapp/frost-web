8c6c611b51988c8c66bcde7eeb98b3ffefd89ad2
Jesse Wicks <33884732+jwicks31@users.noreply.github.com>
2018-11-08 11:17:36 -0600

    feat: Allow ENV Variables  (#419)

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,5 +1,54 @@
+import { map, pipe, fromPairs, keys } from 'ramda'
+
 interface Configuration {
   readonly frostApiUrl: string
 }
 
-export const Configuration: Configuration = require('Configuration')
+export const defaultConfiguration: Configuration = {
+  frostApiUrl: 'http://frost-api:3000',
+}
+
+export const camelCaseToScreamingSnakeCase = (camelCase: string = '') =>
+  camelCase.replace(/([A-Z])/g, capital => '_' + capital).toUpperCase()
+
+const toPair = (s: string) => [camelCaseToScreamingSnakeCase(s), s]
+
+const extractValue = (value: any) => {
+  const coercedValue = value === 'true' ? true : value === 'false' ? false : value
+
+  return isNaN(coercedValue)
+    ? coercedValue
+    : typeof coercedValue === 'boolean'
+      ? coercedValue
+      : parseInt(coercedValue, 10)
+}
+
+export const createEnvToConfigurationKeyMap: (keys: ReadonlyArray<string>) => {
+  readonly [index: string]: string,
+} = pipe(
+  map(toPair),
+  fromPairs,
+)
+
+const loadConfigurationFromEnv = (env: any): Partial<Configuration> => {
+  const map = createEnvToConfigurationKeyMap(keys(defaultConfiguration))
+
+  const configurationFromEnv = Object.entries(env)
+    .filter(([key, value]) => map[key])
+    .filter(([key, value]) => value !== undefined)
+    .reduce(
+      (previousValue, [key, value]: [string, any]) => ({
+        ...previousValue,
+        [map[key]]: extractValue(value),
+      }),
+      {},
+    )
+  return configurationFromEnv
+}
+
+export const getConfigurationWithDefaults = (envVars: any = {}) => ({
+  ...defaultConfiguration,
+  ...loadConfigurationFromEnv(envVars),
+})
+
+export const Configuration = getConfigurationWithDefaults(process.env)
