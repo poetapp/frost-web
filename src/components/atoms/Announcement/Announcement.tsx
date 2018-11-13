df4f725abbc4ce0db8fbab99db29b36b7afe2230
Jesse Wicks <jessewicks@gmail.com>
2018-11-13 16:32:30 -0600

    announcement component

---
+++ src/components/atoms/Announcement/Announcement.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import { Link } from 'react-router'
+
+import './Announcement.scss'
+
+export const Announcement = () => (
+  <div className="Announcement">
+    <Link to="/">
+      Lorem
+    </Link>
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Animi cumque fugiat consequatur perspiciatis optio ab dignissimos in consequuntur.
+      Quo blanditiis ea similique placeat, earum neque at rem tenetur maiores reprehenderit.
+    </div>
+  </div>
+)
--- src/components/molecules/PanelOptions/PanelOptions.tsx
+++ src/components/molecules/PanelOptions/PanelOptions.tsx
@@ -1,4 +1,9 @@
+import { isActiveFeatureName } from '@paralleldrive/feature-toggles'
+import { Feature } from '@paralleldrive/react-feature-toggles'
+
+import { Announcement } from 'components/atoms/Announcement/Announcement'
 import { BoxButton } from 'components/atoms/BoxButton/BoxButton'
+import { FeatureName } from 'config/features'
 import { Images } from 'images/Images'
 import * as React from 'react'
 import { Link } from 'react-router'
@@ -10,6 +15,12 @@ export const PanelOptions = (props: PanelOptionsProps) => (
   <main className={'PanelOptions'}>
     <div className={'PanelOptions__welcome'}>
       <h2>Welcome to Frost</h2>
+      <Feature>
+        {({ features }) =>
+          isActiveFeatureName(FeatureName.Announcement, features) && (
+            <Announcement />
+          )}
+      </Feature>
     </div>
     <div className={'PanelOptions__box-buttons'}>
       <Link to={'/token'}>
