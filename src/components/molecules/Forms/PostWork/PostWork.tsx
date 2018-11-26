8b76d603f85de1e09ffdeb0e870a73b4be5e415c
Jesse Wicks <jessewicks@gmail.com>
2018-11-26 12:59:19 -0600

    made PostWork Form

---
+++ src/components/molecules/Forms/PostWork/PostWork.tsx
@@ -0,0 +1,94 @@
+import * as React from 'react'
+
+import { Input } from 'components/atoms/Input/Input'
+import { Form } from 'components/molecules/Form/Form'
+
+import './PostWork.scss'
+
+interface PostWorkProps {
+  readonly onSubmit: (event: any) => any
+  readonly disabledButton?: boolean
+  readonly serverErrors?: any
+  readonly email: string
+}
+
+interface PostWorkState {
+  readonly name: string
+  readonly author: string
+  readonly datePublished: string
+  readonly dateCreated: string
+  readonly content: string
+}
+
+export class PostWork extends React.Component <PostWorkProps, PostWorkState> {
+  readonly state = {
+    name: 'My First Work!',
+    author: this.props.email,
+    datePublished: new Date().toISOString(),
+    dateCreated: new Date().toISOString(),
+    content: 'Post a work to the Po.et Network!',
+  }
+
+  handleChange(e: any): void {
+    const { name, value } = e.target
+    this.setState(state => ({
+      ...state,
+      [name]: value,
+    }))
+  }
+
+  render(): JSX.Element {
+    const { onSubmit, disabledButton, email } = this.props
+    return (
+      <div className={'PostWork'}>
+        <Form
+          legend={''}
+          onSubmit={onSubmit}
+          textButton={'Post Work'}
+          disabledButton={disabledButton}
+        >
+          <Input
+            name={'name'}
+            type={'text'}
+            placeholder={this.state.name}
+            required
+            className="PostWork__name"
+            onChange={this.handleChange.bind(this)}
+          />
+          <Input
+            name={'author'}
+            type={'text'}
+            placeholder={this.state.author}
+            required
+            className="PostWork__author"
+            onChange={this.handleChange.bind(this)}
+          />
+          <Input
+            name={'datePublished'}
+            type={'text'}
+            placeholder={this.state.datePublished}
+            required
+            className={'PostWork__datePublished'}
+            onChange={this.handleChange.bind(this)}
+          />
+          <Input
+            name={'dateCreated'}
+            type={'text'}
+            placeholder={this.state.dateCreated}
+            required
+            className={'PostWork__dateCreated'}
+            onChange={this.handleChange.bind(this)}
+          />
+          <Input
+            name={'content'}
+            type={'text-area'}
+            placeholder={this.state.content}
+            required
+            className={'PostWork__content'}
+            onChange={this.handleChange.bind(this)}
+          />
+        </Form>
+      </div>
+    )
+  }
+}
