74944082e876477e1363c9fdeea87ea89b296bfc
Jesse Wicks <jessewicks@gmail.com>
2018-11-27 09:22:54 -0600

    added period + flexbox

--- src/components/atoms/Announcement/Announcement.tsx
+++ src/components/atoms/Announcement/Announcement.tsx
@@ -9,7 +9,7 @@ export const Announcement = () =>
       As of today you can now record your creative works on the
       Bitcoin Mainnet, securing the proof of their existence on
       the world's most robust blockchain. To get started, please
-      see our <a href="https://docs.poetnetwork.net/use-poet/beta-user-migration.html">beta user migration guide</a>
+      see our <a href="https://docs.poetnetwork.net/use-poet/beta-user-migration.html">beta user migration guide</a>.
     </p>
     <p>
       This beta platform will be closed on January 15, 2019, so please switch over to the new platform before then.
