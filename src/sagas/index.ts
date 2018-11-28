3b259c286e177decaa3ce778586a55457899d7da
Jesse Wicks <jessewicks@gmail.com>
2018-11-28 15:04:27 -0600

    lint:fix

--- src/sagas/index.ts
+++ src/sagas/index.ts
@@ -4,13 +4,13 @@ import { DeleteApiTokenSaga } from './DeleteApiToken.saga'
 import { ForgotPasswordSaga } from './ForgotPassword.saga'
 import { GetApiTokensSaga } from './GetApiTokens.saga'
 import { GetProfileSaga } from './GetProfile.saga'
-import { WorkClaimFormSaga } from './WorkClaimForm.saga'
 import { RouterSaga } from './Router.sagas'
 import { SendEmailVerifiedAccountSaga } from './SendEmailVerifiedAccount.saga'
 import { SignInSaga } from './SignIn.saga'
 import { SignOutSaga } from './SignOut.saga'
 import { SignUpSaga } from './SignUp.saga'
 import { VerifiedAccountSaga } from './VerifiedAccount.saga'
+import { WorkClaimFormSaga } from './WorkClaimForm.saga'
 export const sagas: ReadonlyArray<any> = [
   SignUpSaga,
   SignInSaga,
